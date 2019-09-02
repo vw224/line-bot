ebd76ac26dd3589f12633df5b848798f6af8cb46
Vivi <vvw224@gmail.com>
2019-09-02 14:58:46 +0800

    add sticker

--- app.py
+++ app.py
@@ -40,6 +40,18 @@ def handle_message(event):
     msg = event.message.text
     r = '我看不懂你說什麼'
 
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+
+        return
+
     if msg in ['hi', 'HI']:
         r = 'hi'
     elif msg == '你吃飯了嗎?':
